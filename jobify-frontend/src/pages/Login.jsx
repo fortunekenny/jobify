9456fbb1446caeeffba17cd8abb51b9382ba8ef3
fortunekenny <gbephraim@gmail.com>
2023-10-29 20:47:03 +0100

    first commit

---
+++ jobify-frontend/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
+
+const Login = () => {
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore app
+        </button>
+        <p>
+          Not yet a member?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login;
