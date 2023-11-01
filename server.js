9a20fec872465dbd372f086cea38b809d70dfd8b
fortunekenny <gbephraim@gmail.com>
2023-11-01 20:36:48 +0100

    validator setup

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 import morgan from "morgan";
 import mongoose from "mongoose";
+import { body, validationResult } from "express-validator";
 
 //
 import jobRouter from "./routes/jobRouter.js";
@@ -29,10 +30,22 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data received", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  [body("name").notEmpty().withMessage("name is required")],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ msg: `hello ${name}` });
+  }
+);
 
 app.use("/api/v1/jobs", jobRouter);
 
