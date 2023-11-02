cac5250ab72110ad374a7a3584f160d76bfc393b
fortunekenny <gbephraim@gmail.com>
2023-11-02 15:27:48 +0100

    authentication

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 //
 import jobRouter from "./routes/jobRouter.js";
@@ -16,11 +17,13 @@ import authRouter from "./routes/authRouter.js";
 //MIDDLEWARE
 
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 //MIDDLEWARE ENDS
@@ -29,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 // Not Found Middleware
