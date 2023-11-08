8988399b4ebeb9b6a179e27768f4b60256f7e20c
fortunekenny <gbephraim@gmail.com>
2023-11-08 08:58:58 +0100

    button refactor

--- jobify-frontend/src/pages/Login.jsx
+++ jobify-frontend/src/pages/Login.jsx
@@ -2,11 +2,10 @@ import {
   Link,
   Form,
   redirect,
-  useNavigation,
   // useActionData,
 } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { FormRow, Logo } from "../components";
+import { FormRow, Logo, SubmitBtn } from "../components";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 
@@ -30,9 +29,6 @@ export const action = async ({ request }) => {
 };
 
 const Login = () => {
-  const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
-
   // const errors = useActionData();
 
   return (
@@ -43,9 +39,7 @@ const Login = () => {
         {/* {errors && <p style={{ color: "red" }}>{errors.msg}</p>} */}
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
-          {isSubmitting ? "Submitting..." : "Submit"}
-        </button>
+        <SubmitBtn formBtn />
         <button type="button" className="btn btn-block">
           explore app
         </button>
