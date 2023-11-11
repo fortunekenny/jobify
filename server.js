d9537a2d490e6a6c7e4850460159cbe39f286aee
fortunekenny <gbephraim@gmail.com>
2023-11-11 17:22:00 +0100

    production

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-// app.use(express.static(path.resolve(__dirname, "./public")));
-app.use(express.static(path.resolve(__dirname, "./jobify-frontend/dist")));
+app.use(express.static(path.resolve(__dirname, "./public")));
+// app.use(express.static(path.resolve(__dirname, "./jobify-frontend/dist")));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -58,8 +58,8 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "./public", "index.html"));
-  res.sendFile(path.resolve(__dirname, "./jobify-frontend/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  // res.sendFile(path.resolve(__dirname, "./jobify-frontend/dist", "index.html"));
 });
 
 // Not Found Middleware
